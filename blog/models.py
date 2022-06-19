410a003deb07f4f418332ef763d6fe3433521af7
Alice Campkin <alice.ridgway@zoodigital.com>
2022-06-19 17:34:07 +0100

    adds post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from autoslug import AutoSlugField
+
 # Create your models here.
+
+class Post(models.Model):
+
+    STATUS_CHOICES = [
+        ("draft", "Draft"),
+        ("published", "Published"),
+    ]
+
+    title = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from="title")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="draft")
+
+    body = models.TextField()
+
+    created_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True)
+    updated_date = models.DateTimeField(blank=True)
+    
