21a4bb87baebd3b5607f24e203f347522088681d
Alice Campkin <alice.ridgway@zoodigital.com>
2022-06-19 18:03:52 +0100

    set up templates/static

--- projectconfig/settings.py
+++ projectconfig/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'projectconfig.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
