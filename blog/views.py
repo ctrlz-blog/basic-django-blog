e98fcaca7a9a994c643eb6fc7d6012cfc2d85658
Alice Campkin <alice.ridgway@zoodigital.com>
2022-06-19 18:04:11 +0100

    adds index view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+
+    published_posts = Post.objects.filter(status="published")
+
+    context = {
+        "posts": published_posts
+    }
+
+    return render(request, "index.html", context)
