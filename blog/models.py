1094c04ed492e3fc95d42b4e52d254a1f37c93bf
Alice Campkin <alice.campkin@gmail.com>
2022-06-20 21:52:03 +0100

    adds excerpt

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from autoslug import AutoSlugField
 
 # Create your models here.
 
+
 class Post(models.Model):
 
     STATUS_CHOICES = [
@@ -13,11 +14,25 @@ class Post(models.Model):
 
     title = models.CharField(max_length=255)
     slug = AutoSlugField(populate_from="title")
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="draft")
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="draft"
+    )
 
     body = models.TextField()
 
-    created_date = models.DateTimeField(auto_now=True)
-    published_date = models.DateTimeField(blank=True)
-    updated_date = models.DateTimeField(blank=True)
-    
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    updated_date = models.DateTimeField(blank=True, null=True)
+
+    @property
+    def excerpt(self):
+        character_limit = 150
+        if len(self.body) < character_limit:
+            excerpt = self.body
+        else:
+            excerpt = self.body[:character_limit]+"..."
+
+        return excerpt
+
+    def __str__(self):
+        return self.title
